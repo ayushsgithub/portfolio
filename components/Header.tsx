819b4bfdc1984648e6cec474d5ffa86840197c49
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-05-13 12:58:06 +0530

    leetcode link added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 type Props = {
@@ -18,7 +20,7 @@ export default function Header( props: Props) {
         <SocialIcon className='hover:-translate-y-1 ease-in duration-100' target='_blank' url="https://twitter.com/ayushisreal" network='twitter' fgColor='teal' bgColor='transparent' />
         <SocialIcon className='hover:-translate-y-1 ease-in duration-100' target='_blank' url="https://linkedin.com/in/ayushslink" network='linkedin' fgColor='lightblue' bgColor='transparent' />
         <SocialIcon className='hover:-translate-y-1 ease-in duration-100' target='_blank' url="https://github.com/ayushsgithub" network='github' fgColor='white' bgColor='transparent' />
-        <SocialIcon className='hover:-translate-y-1 ease-in duration-100' target='_blank' url="https://t.me/8804772137" network='telegram' fgColor='white' bgColor='transparent' />
+        <Link href={"https://leetcode.com/u/ayushchaurasiya78/"} target='_blank' className='hover:-translate-y-1 ease-in duration-100'><Image src='/skillPhoto/leetcode.png' width={35} height={35} alt='logo' /></Link>
         </motion.div>
 
         <motion.div 
@@ -26,8 +28,8 @@ export default function Header( props: Props) {
         animate={{ x: 0, opacity: 1, scale: 1}}
         transition={{duration: 1.2,}}
         className='flex flex-row items-center text-gray-300 '>
-            <SocialIcon className='hover:-translate-y-1 ease-in duration-100 cursor-pointer' target='_blank' url='mailto:ayushchaurasiya78@gmail.com' network='mailto' fgColor='gray' bgColor='transparent'/>
-            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+            <SocialIcon className='hover:-translate-y-1 ease-in duration-100 cursor-pointer' target='_blank' url='mailto:ayushchaurasiya78@gmail.com' network='mailto' fgColor='#D3D3D3' bgColor='transparent'/>
+            <p className='uppercase hidden md:inline-flex text-sm text-#D3D3D3'>Get In Touch</p>
         </motion.div>
     </header>
   )
