471d7c002389e3b4cb239df085bf8fcbddb7cfa1
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-05-24 23:05:30 +0530

    fixed error

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,53 +1,10 @@
-// import React from 'react'
-// import { motion } from 'framer-motion'
-// import ExperienceCard from './ExperienceCard'
-// import { experiences } from '../components/experiences.json'
-
-
-// type Props = {
-//     test: string;
-// }
-
-// const Experience = ({}: Props) => {
-// return (
-//     <motion.div
-//         initial={{ opacity: 0 }}
-//         whileInView={{ opacity: 1 }}
-//         transition={{ duration: 1.5 }}
-//         className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center '>
-//         <h3 className='absolute top-24 md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-//         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
-//             {experiences.map((experience) => (
-//                 <ExperienceCard
-//                     key={experience.id}
-//                     company={experience.name}
-//                     start={experience.date[0]}
-//                     end={experience.date[1]}
-//                     skills={experience.skills}
-//                     points={experience.points}
-//                     role={experience.role}
-//                 />
-//             ))}
-//         </div>
-//     </motion.div>
-// )
-// }
-
-// export default Experience
-
-
-
-
-
 import React from 'react'
 import { motion } from 'framer-motion'
 import ExperienceCard from './ExperienceCard'
 import { experiences } from '../components/experiences.json'
 
 
-type Props = {
-    test: string;
-}
+type Props = {}
 
 const Experience = ({}: Props) => {
 return (
