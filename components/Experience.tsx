6f36797375d96e95a3a427e512a38cf4cffa7baa
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-05-25 12:16:07 +0530

    experience section mod

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,9 +12,10 @@ return (
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
+        className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center '>
         <h3 className='absolute top-24 md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-        <div className='w-full flex space-x-5 '>
+        <div className='overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A] h-full content-center'>
+        <div className='w-full flex space-x-5 h-1/2'>
             {experiences.map((experience) => (
                 <ExperienceCard
                     key={experience.id}
@@ -26,6 +27,7 @@ return (
                     role={experience.role}
                 />
             ))}
+        </div>
         </div>
     </motion.div>
 )
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -11,18 +11,18 @@ type Props = {
 
 const ExperienceCard = ({company, start, end, points, skills, role}: Props) => {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-xl items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden justify-center '>
         <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>{role}</h4>
-            <p className='font-bold text-2xl mt-1'>{company}</p>
-            <div className='flex space-x-2 my-2 overflow-hidden'>
+            <h4 className='text-4xl font-light tracking-wider'>{role}</h4>
+            <p className='font-bold text-2xl mt-3 tracking-wider'>{company}</p>
+            <div className='flex space-x-2 my-3 overflow-hidden '>
                 {skills.map((skill, index) => (<img key={index} className='h-10 w-10 rounded-full' src={`/skillPhoto/${skill}`} alt="skill" />))}
             </div>
-            <p className='uppercase py-5 text-gray-300'>{end ? "From " + start + "To " + end : start}</p>
+            <p className='uppercase py-5 text-gray-300 italic font-mono'>{end ? "From " + start + " To " + end : start}</p>
 
-            <ul className='list-disc space-y-4 ml-5 text-lg h-96 pr-7 overflow-y-auto scrollbar-thin scrollbar-track-black/25 scrollbar-thumb-[#F7AB0A]'>
+            {/* <ul className='list-disc space-y-4 ml-5 text-lg h-96 pr-7 overflow-y-auto scrollbar-thin scrollbar-track-black/25 scrollbar-thumb-[#F7AB0A]'>
                 {points.map((point, index) => (<li key={index}>{point}</li>))}
-            </ul>
+            </ul> */}
         </div>
     </article>
   )
