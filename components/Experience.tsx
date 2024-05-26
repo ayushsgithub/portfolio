6c82b994f2b40c68d01d3750b2d3147f76e7126a
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-05-26 12:47:39 +0530

    fixex experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import ExperienceCard from './ExperienceCard'
-import { experiences } from '../components/experiences.json'
+import experiences from './experiences.json'
 
 
 type Props = {}
@@ -12,22 +12,21 @@ return (
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center '>
+        className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
         <h3 className='absolute top-24 md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-        <div className='overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A] h-full content-center'>
-        <div className='w-full flex space-x-5 h-1/2'>
-            {experiences.map((experience) => (
-                <ExperienceCard
-                    key={experience.id}
-                    company={experience.name}
-                    start={experience.date[0]}
-                    end={experience.date[1]}
-                    skills={experience.skills}
-                    points={experience.points}
-                    role={experience.role}
-                />
-            ))}
-        </div>
+
+        <div className='w-full flex space-x-5 h-full items-center relative z-20 overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+        {experiences.experiences.map((experience) => (
+            <ExperienceCard
+                key={experience.id}
+                company={experience.name}
+                start={experience.date[0]}
+                end={experience.date[1]}
+                skills={experience.skills}
+                points={experience.points}
+                role={experience.role}
+            />
+        ))}
         </div>
     </motion.div>
 )
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const ExperienceCard = ({company, start, end, points, skills, role}: Props) => {
   return (
-    <article className='flex flex-col rounded-xl items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden justify-center '>
+    <article className='flex flex-col rounded-xl items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden h-1/3 '>
         <div className='px-0 md:px-10'>
             <h4 className='text-4xl font-light tracking-wider'>{role}</h4>
             <p className='font-bold text-2xl mt-3 tracking-wider'>{company}</p>
