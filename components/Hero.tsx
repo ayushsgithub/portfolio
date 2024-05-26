4b019a0728f6b5490bc80e1e04e3e5b03c558a50
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-05-26 12:51:16 +0530

    experience button added to hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,6 +44,9 @@ export default function Hero({ pageInfo }: Props) {
           <Link href="#skills">
           <button className='heroButton'>Skills</button>
           </Link>
+          <Link href="#experience">
+          <button className='heroButton'>Experience</button>
+          </Link>
           <Link href="#projects">
           <button className='heroButton'>Projects</button>
           </Link>
