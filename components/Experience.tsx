ddce33788e90a21b3460fe59f2077889cd7a0787
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-08-23 16:38:00 +0530

    mod

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -13,7 +13,7 @@ return (
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
         className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
-        <h3 className='absolute top-24 md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
+        <h3 className='absolute top-24 md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experiences</h3>
 
         <div className='w-full flex space-x-5 h-full items-center relative z-20 overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
         {experiences.experiences.map((experience) => (
